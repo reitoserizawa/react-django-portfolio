f04fc940f237e7e279b5ef8a99f9b0650ead4371
ReitoSerizawa <s.reitiger@gmail.com>
2023-02-14 17:51:26 -0500

    loading added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
+import Typewriter from "typewriter-effect";
 import NavBar from "./components/NavBar/NavBar";
 import About from "./components/About/About";
 import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
   return (
     <>
-      <NavBar></NavBar>
-      <About></About>
-      <About></About>
-      <About></About>
-      <About></About>
+      {loading ? (
+        <div id="loading-parent">
+          <div id="loading-child">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Hi, I am Reito Serizawa!")
+                  .deleteAll()
+                  .callFunction(() => setLoading(false))
+                  .start();
+              }}
+            />
+          </div>
+        </div>
+      ) : (
+        <>
+          <NavBar />
+          <About />
+        </>
+      )}
     </>
   );
 }
--- portfolio/src/components/NavBar/NavBar.js
+++ portfolio/src/components/NavBar/NavBar.js
@@ -9,58 +9,58 @@ function NavBar() {
   };
 
   return (
-    <header class="primary-header flex">
+    <header className="primary-header flex">
       <div>
-        <img src={logo} alt="portfolio logo" class="logo" />
+        <img src={logo} alt="portfolio logo" className="logo" />
       </div>
 
       <button
-        class="mobile-nav-toggle"
+        className="mobile-nav-toggle"
         aria-controls="primary-navigation"
         aria-expanded={visibility}
         onClick={navToggle}
       >
         {visibility ? (
-          <i class="fa-solid fa-xmark fa-2x"></i>
+          <i className="fa-solid fa-xmark fa-2x"></i>
         ) : (
-          <i class="fa-solid fa-bars fa-2x"></i>
+          <i className="fa-solid fa-bars fa-2x"></i>
         )}
-        <span class="sr-only">Menu</span>
+        <span className="sr-only">Menu</span>
       </button>
 
       <nav>
         <ul
           id="primary-navigation"
-          class="primary-navigation flex"
+          className="primary-navigation flex"
           data-visible={visibility}
         >
-          <li class="active">
-            <a>
+          <li className="active">
+            <a href="#home">
               <span aria-hidden="true">00</span>Home
             </a>
           </li>
-          <li class="active">
-            <a>
+          <li className="active">
+            <a href="#about">
               <span aria-hidden="true">01</span>About
             </a>
           </li>
-          <li class="active">
-            <a>
+          <li className="active">
+            <a href="#work">
               <span aria-hidden="true">02</span>Work
             </a>
           </li>
-          <li class="active">
-            <a>
+          <li className="active">
+            <a href="#blog">
               <span aria-hidden="true">03</span>Blog
             </a>
           </li>
-          <li class="active">
-            <a>
+          <li className="active">
+            <a href="#contact">
               <span aria-hidden="true">04</span>Contact
             </a>
           </li>
-          <li class="active">
-            <a>
+          <li className="active">
+            <a href="#resume">
               <span aria-hidden="true">05</span>Resume
             </a>
           </li>
