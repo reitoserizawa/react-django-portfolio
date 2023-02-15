84e41f97a9e63affbd10ed20cd4d4c29b5ff346f
ReitoSerizawa <s.reitiger@gmail.com>
2023-02-15 18:05:44 -0500

    feature-home done

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -7,27 +7,31 @@ function Home() {
       <div class="home__container container grid">
         <div class="home__content grid">
           <div class="home__social">
-            <a
-              class="home__social-icon"
-              href="https://www.linkedin.com/in/reitos/"
-              tagret="_blank"
-            >
-              <i class="fa-brands fa-linkedin-in"></i>
-            </a>
-            <a
-              class="home__social-icon"
-              href="https://github.com/reitoserizawa"
-              tagret="_blank"
-            >
-              <i class="fa-brands fa-github"></i>
-            </a>
-            <a
-              class="home__social-icon"
-              href="https://medium.com/@s.reitiger"
-              tagret="_blank"
-            >
-              <i class="fa-brands fa-medium"></i>
-            </a>
+            <div class="thought">
+              <div class="home__social-list">
+                <a
+                  class="home__social-icon"
+                  href="https://www.linkedin.com/in/reitos/"
+                  tagret="_blank"
+                >
+                  <i class="fa-brands fa-linkedin-in fa-xl"></i>
+                </a>
+                <a
+                  class="home__social-icon"
+                  href="https://github.com/reitoserizawa"
+                  tagret="_blank"
+                >
+                  <i class="fa-brands fa-github fa-xl"></i>
+                </a>
+                <a
+                  class="home__social-icon"
+                  href="https://medium.com/@s.reitiger"
+                  tagret="_blank"
+                >
+                  <i class="fa-brands fa-medium fa-xl"></i>
+                </a>
+              </div>
+            </div>
           </div>
           <div class="home__blob">
             <img class="home__blob-img" src={image} />
